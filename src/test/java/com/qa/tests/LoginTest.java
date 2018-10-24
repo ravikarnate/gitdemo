60a63bec497cda361be623c69a0469d25c283287
ravindranath <ravindranath@LSLT-IT10255>
2018-10-24 18:51:48 +0530

    Comminting the code

--- src/test/java/com/qa/tests/LoginTest.java
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -1,12 +1,35 @@
 package com.qa.tests;
 
+import java.sql.Driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class LoginTest {
-	
+
+	WebDriver driver;
+	@BeforeTest
+	public void url() {
+		
+		System.setProperty("webdriver.chrome.driver","/home/ravindranath/Downloads/chromedriver_linux64/chromedriver");
+		
+		driver = new ChromeDriver();
+		
+		driver.get("http://demo.automationtesting.in/Index.html");
+	}
 	@Test
 	public void test1 () {
-		System.out.println("test1 method");
+		
+		
+	}
+	
+	@AfterTest
+	public void close() {
+	
+		driver.close();
 	}
 
 }
